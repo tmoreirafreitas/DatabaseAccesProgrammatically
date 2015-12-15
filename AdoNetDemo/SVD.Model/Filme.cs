820276a82ea75c2f0c212fc22858c2fae856b0a7
tmoreirafreitas <tmoreirafreitas@hotmail.com>
2015-12-15 20:55:38 -0200

    Adição da propriedade AtuacoesAtores do tipo List<Ator>

--- AdoNetDemo/SVD.Model/Filme.cs
+++ AdoNetDemo/SVD.Model/Filme.cs
@@ -7,11 +7,12 @@ namespace SVD.Model
 {
     public class Filme
     {
-        public int ID { get; set; }
-        public Genero Genero { get; set; }
-        public Categoria Categoria { get; set; }
+        public int ID { get; set; }        
         public string Titulo { get; set; }
         public string Duracao { get; set; }
+        public Genero Genero { get; set; }
+        public Categoria Categoria { get; set; }
         public List<Copia> Copias { get; set; }
+        public List<Ator> AtuacoesAtores { get; set; }
     }
 }
