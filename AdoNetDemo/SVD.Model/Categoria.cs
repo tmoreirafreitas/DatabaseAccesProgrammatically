8fbbb185e80568a7819ddafa15befb0af35e3a2a
tmoreirafreitas <tmoreirafreitas@hotmail.com>
2015-12-18 07:40:15 -0200

    Correção da propriedade filmes

--- AdoNetDemo/SVD.Model/Categoria.cs
+++ AdoNetDemo/SVD.Model/Categoria.cs
@@ -11,7 +11,7 @@ namespace SVD.Model
         public int?        ID           { get; set; }
         public string      Descricao    { get; set; }
         public decimal     ValorLocacao { get; set; }
-        public List<Filme> Filmes       { get { return new List<Filme>(); } private set; }
+        public List<Filme> Filmes       { get { return new List<Filme>(); } }
 
         public bool AddFilme(Filme filme)
         {
