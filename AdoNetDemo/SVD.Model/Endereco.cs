b909d37a0efee5ef36ee3c6bb5f3c2e1c9d6b60e
tmoreirafreitas <tmoreirafreitas@hotmail.com>
2015-12-18 08:12:33 -0200

    Mudança da propriedade CEP do tipo char para string

--- AdoNetDemo/SVD.Model/Endereco.cs
+++ AdoNetDemo/SVD.Model/Endereco.cs
@@ -12,7 +12,7 @@ namespace SVD.Model
         public string       Rua          { get; set; }
         public int          Numero       { get; set; }
         public string       Complemento  { get; set; }
-        public char         CEP          { get; set; }
+        public string       CEP          { get; set; }
         public string       Bairro       { get; set; }
         public string       Cidade       { get; set; }
         public string       Estado       { get; set; }
