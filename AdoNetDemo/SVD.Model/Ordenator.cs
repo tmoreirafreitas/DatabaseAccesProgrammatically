91581e99f447172df53a33465ccc67b78354adb9
tmoreirafreitas <tmoreirafreitas@hotmail.com>
2016-01-22 09:22:09 -0200

    22/01/2016 - Criação da classe de ordenação "Ordenator"

---
+++ AdoNetDemo/SVD.Model/Ordenator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SVD.Model
+{
+    public class Ordenator<T> : IComparer<T>
+    {
+        PropertyInfo propriedade;
+        bool ascendente;
+
+        public Ordenator(string nomePropriedade, bool ascendente)
+        {
+            propriedade = typeof(T).GetProperty(nomePropriedade);
+            this.ascendente = ascendente;
+        }
+
+        public int Compare(T x, T y)
+        {
+            if (ascendente)
+            {
+                return ((IComparable)propriedade.GetValue(x, null)).CompareTo(propriedade.GetValue(y, null));
+            }
+            else
+            {
+                return ((IComparable)propriedade.GetValue(y, null)).CompareTo(propriedade.GetValue(x, null));
+            }
+        }
+    }
+}
